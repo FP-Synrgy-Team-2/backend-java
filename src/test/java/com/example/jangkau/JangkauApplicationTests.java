8e77eeb21a24ea054312b4b459df4c24a241004c
khrnnisak <khrnnisak07@gmail.com>
2024-07-11 12:11:09 +0700

    first commit

---
+++ src/test/java/com/example/jangkau/JangkauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jangkau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangkauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
