7fbb3be2fecd6e07ddd5f7322535550d2402676d
yahya ayyas <135834292+yahyaayyas1@users.noreply.github.com>
2024-07-17 14:12:41 +0700

    Update JangkauApplication.java

--- src/main/java/com/example/jangkau/JangkauApplication.java
+++ src/main/java/com/example/jangkau/JangkauApplication.java
@@ -1,6 +1,7 @@
 package com.example.jangkau;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
