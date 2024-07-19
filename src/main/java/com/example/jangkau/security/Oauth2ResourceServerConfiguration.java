4d4a484847ed3e14456411d54206fe36f755be47
khrnnisak <khrnnisak07@gmail.com>
2024-07-19 23:20:41 +0700

    update

--- src/main/java/com/example/jangkau/security/Oauth2ResourceServerConfiguration.java
+++ src/main/java/com/example/jangkau/security/Oauth2ResourceServerConfiguration.java
@@ -28,6 +28,7 @@ public class Oauth2ResourceServerConfiguration extends ResourceServerConfigurerA
             "/transactions",
             "/transactions/**",
             "/bank-accounts",
+            "/bank-accounts/**",
             "/saved-accounts/**"
     };
 
