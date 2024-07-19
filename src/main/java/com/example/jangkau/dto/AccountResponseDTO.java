a0d2f90302893a97e7aa044bdf2e943fbd5c211a
Lukman <lckmnzans@gmail.com>
2024-07-19 23:00:11 +0700

    changes in AccountController

--- src/main/java/com/example/jangkau/dto/AccountResponseDTO.java
+++ src/main/java/com/example/jangkau/dto/AccountResponseDTO.java
@@ -3,11 +3,13 @@ package com.example.jangkau.dto;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class AccountResponseDTO {
     private UUID userId;
     private String ownerName;
