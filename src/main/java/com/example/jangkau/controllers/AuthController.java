06f39dfb55016acc8a4745bb69ce3a9dc91f38e9
yahyaayyas1 <yahyaayyas112@gmail.com>
2024-07-29 01:19:41 +0700

    refresh token

--- src/main/java/com/example/jangkau/controllers/AuthController.java
+++ src/main/java/com/example/jangkau/controllers/AuthController.java
@@ -5,12 +5,19 @@ import com.example.jangkau.dto.base.BaseResponse;
 import com.example.jangkau.services.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
+import java.util.Map;
 
 @Tag(name = "Auth")
 @RestController
@@ -19,6 +26,12 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+
+    @Value("${BASEURL}")
+    private String authUrl;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
 
     @Autowired
     private TokenStore tokenStore;
@@ -29,15 +42,11 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+    public ResponseEntity<BaseResponse<LoginResponse>> login(@RequestBody LoginRequest request, HttpServletResponse response) {
         LoginResponse loginResponse = authService.login(request, response);
-        return ResponseEntity.ok(loginResponse);
+        BaseResponse<LoginResponse> baseResponse = BaseResponse.success(loginResponse, "Success Login User");
+        return ResponseEntity.ok(baseResponse);
     }
-
-//    @PostMapping("login")
-//    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-//        return ResponseEntity.ok(BaseResponse.success(authService.login(request), "Success Login User"));
-//    }
 
 //    @PostMapping("/logout")
 //    public void logout(HttpServletRequest request, Authentication authentication) {
--- src/main/java/com/example/jangkau/resources/DummyResource.java
+++ src/main/java/com/example/jangkau/resources/DummyResource.java
@@ -2,15 +2,15 @@ package com.example.jangkau.resources;
 
 public class DummyResource {
     static String[] ACCOUNTS_NUMBER = new String[] {
-            "44441234561","44441244631"
+            "44441234561","44441244631","44441234567"
     };
     static Double[] ACCOUNTS_BALANCE = new Double[] {
-            1000000.0,2000000.0
+            1000000.0,2000000.0,2000000.0
     };
     static String[] ACCOUNTS_OWNER_NAME = new String[] {
             "Full Name Admin", "Full Name User"
     };
     static int[] ACCOUNTS_PIN = new int[] {
-            123456,135797
+            123456,135797,654321
     };
 }
