a129147926555533b9fcc315936a8010b4ff7e66
yahyaayyas1 <yahyaayyas112@gmail.com>
2024-08-19 12:05:50 +0700

    update

--- src/main/java/com/example/jangkau/dto/MerchantResponse.java
+++ src/main/java/com/example/jangkau/dto/MerchantResponse.java
@@ -12,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class MerchantResponse {
     private String id;
     private String name;
+    private String accountId;
 }
--- src/main/java/com/example/jangkau/mapper/MerchantMapper.java
+++ src/main/java/com/example/jangkau/mapper/MerchantMapper.java
@@ -1,6 +1,7 @@
 package com.example.jangkau.mapper;
 
 import com.example.jangkau.dto.MerchantResponse;
+import com.example.jangkau.models.Account;
 import com.example.jangkau.models.Merchant;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ public class MerchantMapper {
         return MerchantResponse.builder()
                 .id(merchant.getId().toString())
                 .name(merchant.getName())
+                .accountId(merchant.getAccount() != null ? merchant.getAccount().getId().toString() : null)
                 .build();
     }
 }
