0e9ea5e219488b5bd65e32015318340d90a66cbc
yahya ayyas <135834292+yahyaayyas1@users.noreply.github.com>
2024-08-25 13:41:53 +0700

    Update TransactionRepository.java

--- src/main/java/com/example/jangkau/repositories/TransactionRepository.java
+++ src/main/java/com/example/jangkau/repositories/TransactionRepository.java
@@ -16,28 +16,32 @@ import org.springframework.stereotype.Repository;
 import com.example.jangkau.models.Transactions;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transactions, UUID>{
-    @Query(value = "select * from transactions\n" +
-            "where (account_id = ?1 or beneficiary_account = ?1) and deleted_date is null\n" +
-            "order by transaction_date desc", nativeQuery = true)
+public interface TransactionRepository extends JpaRepository<Transactions, UUID> {
+    @Query(value = "SELECT * FROM transactions " +
+            "WHERE (account_id = ?1 OR beneficiary_account = ?1) AND deleted_date IS NULL " +
+            "ORDER BY transaction_date DESC", nativeQuery = true)
     List<Transactions> findAllTransactionsHistoryByAccount(UUID accountId);
 
     List<Transactions> findByBeneficiaryAccount(Account beneficiaryAccount);
-    
+
     List<Transactions> findByAccountIdOrBeneficiaryAccount(Account accountId, Account beneficiaryAccount, Pageable pageable);
 
-    @Query(value = "select * from transactions\n"+
-        "where (account_id = ?1 or beneficiary_account = ?1) and DATE(transaction_date) >= ?2 and DATE(transaction_date) <= ?3 and deleted_date is null\n"+
-        "order by transaction_date DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM transactions " +
+            "WHERE (account_id = ?1 OR beneficiary_account = ?1) " +
+            "AND transaction_date >= ?2 " +
+            "AND transaction_date <= ?3 " +
+            "AND deleted_date IS NULL " +
+            "ORDER BY transaction_date DESC", nativeQuery = true)
     List<Transactions> findAllTransactionsByDate(UUID accountId, Date startDate, Date endDate);
 
-    @Query(value = "select * from transactions\n"+
-    "where (account_id = ?1 or beneficiary_account = ?1) and deleted_date is null\n"+
-    "order by transaction_date DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM transactions " +
+            "WHERE (account_id = ?1 OR beneficiary_account = ?1) AND deleted_date IS NULL " +
+            "ORDER BY transaction_date DESC", nativeQuery = true)
     List<Transactions> findAllTransactions(UUID accountId);
 
-    @Query(value = "select * from transactions\n"+
-    "where (account_id = ?1 or beneficiary_account = ?1) and DATE(transaction_date) = ?2 and deleted_date is null\n"+
-    "order by transaction_date DESC", nativeQuery = true)
+    @Query(value = "select * from transactions\n" +
+            "where (account_id = ?1 or beneficiary_account = ?1) and DATE(transaction_date) = DATE(?2) and deleted_date is null\n" +
+            "order by transaction_date DESC", nativeQuery = true)
     List<Transactions> findNowTransactions(UUID accountId, Date date);
-} 
+
+}
